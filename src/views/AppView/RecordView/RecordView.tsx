ee5b8450d14bedc4711da10abb34e2cea7c1545d
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-05-30 19:59:41 -0500

    non-functional recording processing menu done

--- src/views/AppView/RecordView/RecordView.tsx
+++ src/views/AppView/RecordView/RecordView.tsx
@@ -1,3 +1,73 @@
+import { Icon } from "@iconify/react";
+import "./RecordView.scss";
+import { useEffect, useState } from "react";
+
 export default function RecordView() {
-  return <div>RecordView</div>;
+  const [isRecording, setIsRecording] = useState(false);
+  const [inputRecording, setInputRecording] = useState(false);
+
+  const processingOptions = [
+    {
+      name: "retune it",
+      icon: "fluent:wand-16-filled",
+    },
+    {
+      name: "add accompaniment",
+      icon: "emojione-monotone:musical-notes",
+    },
+    {
+      name: "turn it into drums",
+      icon: "fa6-solid:drum",
+    },
+    {
+      name: "keep it as is",
+      icon: "bx:cool",
+    },
+  ];
+
+  useEffect(() => {
+    if (isRecording) {
+      setTimeout(() => {
+        setIsRecording(false);
+        setInputRecording(true);
+      }, 2000);
+    }
+  }, [isRecording]);
+  return (
+    <div className="RecordView">
+      {!inputRecording ? (
+        !isRecording ? (
+          <div className="record-button" onClick={() => setIsRecording((x) => !x)}>
+            <Icon className="icon" icon="fluent:record-24-regular" />
+            <span className="text">press to record!</span>
+          </div>
+        ) : (
+          <span className="recording-animation">[placeholder for recording animation]</span>
+        )
+      ) : (
+        <div className="post-recording-subview">
+          <div className="input-recording">
+            <div className="waveform">
+              {[...Array(20).keys()].map((key) => (
+                <div key={key}>
+                  <Icon className="icon" icon="ph:waveform-bold" />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="menu">
+            <div className="description">What would you like to do with your recording?</div>
+            <div className="options">
+              {processingOptions.map((opt) => (
+                <div className="option" key={opt.name}>
+                  <Icon className="icon" icon={opt.icon} />
+                  <span className="text">{opt.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
