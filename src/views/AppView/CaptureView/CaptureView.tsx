1033303f778b0ab1e2a1d0533671f410f3aeecff
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-06-14 17:11:11 -0500

    reverting back to pre-test — issue was blackhole audio routing

--- src/views/AppView/CaptureView/CaptureView.tsx
+++ src/views/AppView/CaptureView/CaptureView.tsx
@@ -1,14 +1,14 @@
 import Icon from "../../../components/Icon/Icon";
-// import { useAppSelector } from "../../../redux/hooks";
+import { useAppSelector } from "../../../redux/hooks";
 import icons from "../../../utils/icons";
 import "./CaptureView.scss";
 import MicrophoneView from "./MicrophoneView/MicrophoneView";
 
 export default function CaptureView() {
-  // const { isPreprocessing } = useAppSelector((state) => state.mic);
+  const { isPreprocessing } = useAppSelector((state) => state.mic);
   return (
     <div className="CaptureView">
-      {false ? (
+      {isPreprocessing ? (
         <div className="CaptureView__processing">
           <Icon className="CaptureView__processing__icon" icon={icons.processing} />
         </div>
