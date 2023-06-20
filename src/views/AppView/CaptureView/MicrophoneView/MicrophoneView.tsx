a6dcf45fea1fa8aef1172b119c8c959f9416162b
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-06-20 14:04:15 -0500

    Add time-warping API endpoint on user recordings

--- src/views/AppView/CaptureView/MicrophoneView/MicrophoneView.tsx
+++ src/views/AppView/CaptureView/MicrophoneView/MicrophoneView.tsx
@@ -29,25 +29,40 @@ const MicrophoneView = () => {
   const { tempo } = useAppSelector((state) => state.mic);
 
   useEffect(() => {
-    if (!isRecording && audioBlob) {
+    async function processRecording() {
+      if (isRecording || !audioBlob) {
+        return;
+      }
       dispatch(micActions.togglePreprocessing());
-      getAudioDuration(audioBlob)
-        .then((duration) => {
-          encodeBlobAsWav(audioBlob).then((blob) => {
-            const rec = {
-              url: URL.createObjectURL(blob),
-              name: recTitle || getFormattedTimestamp(),
-              duration: duration,
-              features: {
-                tempo: tempo,
-              },
-            };
-            dispatch(recordingActions.addNew(rec));
-            navigate("/app/play/recordings/0/develop");
-          });
-        })
-        .finally(() => dispatch(micActions.togglePreprocessing()));
+      try {
+        const wavBlob = await encodeBlobAsWav(audioBlob);
+        const formData = new FormData();
+        formData.append("file", wavBlob);
+        const response = await fetch(`http://18.212.195.169:8000/generate/constanttempo/?user_tempo=${tempo}`, {
+          method: "PUT",
+          body: formData,
+        });
+
+        const processedBlob = await response.blob();
+
+        const duration = await getAudioDuration(processedBlob);
+        const rec = {
+          url: URL.createObjectURL(processedBlob),
+          name: recTitle || getFormattedTimestamp(),
+          duration: duration,
+          features: {
+            tempo: tempo,
+          },
+        };
+        dispatch(recordingActions.addNew(rec));
+        navigate("/app/play/recordings/0/develop");
+      } catch {
+      } finally {
+        dispatch(micActions.togglePreprocessing());
+      }
     }
+
+    processRecording();
   }, [isRecording]);
 
   return (
