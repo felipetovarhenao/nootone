d1e394de019be44c681d7d6ebe4d7b4dd6a0f835
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-07-15 09:40:11 -0500

    Add sliding animation to SwipeMenu

--- src/components/SwipeMenu/SwipeMenu.tsx
+++ src/components/SwipeMenu/SwipeMenu.tsx
@@ -1,5 +1,5 @@
 import "./SwipeMenu.scss";
-import React, { ReactElement, ReactNode, useEffect, useState } from "react";
+import React, { ReactElement, ReactNode, useEffect, useRef, useState } from "react";
 import { useSwipeable } from "react-swipeable";
 import Icon from "../Icon/Icon";
 import icons from "../../utils/icons";
@@ -15,9 +15,11 @@ type SwipeMenuProps = {
 
 const SwipeMenu = ({ className, children, defaultValue = 0, onSwiped }: SwipeMenuProps) => {
   const [selected, setSelected] = useState(defaultValue);
+  const directionRef = useRef<"LEFT" | "RIGHT" | "NEUTRAL">("NEUTRAL");
 
   function handleSwipe(num: number) {
-    const newIndex = wrapValue(selected - num, React.Children.count(children));
+    const newIndex = wrapValue(selected + num, React.Children.count(children));
+    directionRef.current = num === 1 ? "LEFT" : "RIGHT";
     setSelected(newIndex);
     if (onSwiped) {
       onSwiped(newIndex);
@@ -26,10 +28,10 @@ const SwipeMenu = ({ className, children, defaultValue = 0, onSwiped }: SwipeMen
 
   const handlers = useSwipeable({
     onSwipedLeft: () => {
-      handleSwipe(-1);
+      handleSwipe(1);
     },
     onSwipedRight: () => {
-      handleSwipe(1);
+      handleSwipe(-1);
     },
     trackTouch: true,
     trackMouse: true,
@@ -38,7 +40,10 @@ const SwipeMenu = ({ className, children, defaultValue = 0, onSwiped }: SwipeMen
   const showSelected = () => {
     return React.Children.map(children as any, (child: ReactElement, i: number) => {
       if (selected === i) {
-        return child;
+        return React.cloneElement(child, {
+          ...child.props,
+          className: cn(child.props.className, `SwipeMenu__${directionRef.current.toLowerCase()}`),
+        });
       }
     });
   };
@@ -49,9 +54,9 @@ const SwipeMenu = ({ className, children, defaultValue = 0, onSwiped }: SwipeMen
 
   return (
     <div className={cn(className, "SwipeMenu")} {...handlers}>
-      <Icon className="SwipeMenu__button" icon={icons.left} onClick={() => handleSwipe(1)} />
+      <Icon className="SwipeMenu__button" icon={icons.left} onClick={() => handleSwipe(-1)} />
       {showSelected()}
-      <Icon className="SwipeMenu__button" icon={icons.right} onClick={() => handleSwipe(-1)} />
+      <Icon className="SwipeMenu__button" icon={icons.right} onClick={() => handleSwipe(1)} />
     </div>
   );
 };
