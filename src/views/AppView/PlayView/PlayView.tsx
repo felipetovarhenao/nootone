7eb1bbb743d148ac4cd4c759e162502dfd2eabf1
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-07-19 14:02:15 -0500

    minor edit

--- src/views/AppView/PlayView/PlayView.tsx
+++ src/views/AppView/PlayView/PlayView.tsx
@@ -4,6 +4,6 @@ import NoTracksView from "./NoTracksView/NoTracksView";
 import MyIdeasView from "./MyIdeasView/MyIdeasView";
 
 export default function PlayView() {
-  const { recordings: savedRecordings } = useAppSelector((state) => state.recordings);
-  return <div className="PlayView">{savedRecordings.length === 0 ? <NoTracksView /> : <MyIdeasView />}</div>;
+  const { recordings } = useAppSelector((state) => state.recordings);
+  return <div className="PlayView">{recordings.length === 0 ? <NoTracksView /> : <MyIdeasView />}</div>;
 }
