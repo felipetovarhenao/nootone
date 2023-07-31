4398ab153db5934977c8b6733f88e997fd36ccfd
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-07-31 18:31:09 -0500

    Add more test recordings

--- src/utils/loadTestRecordings.ts
+++ src/utils/loadTestRecordings.ts
@@ -1,7 +1,7 @@
 import getAudioSpecs from "./getAudioSpecs";
 
 export default function loadTestRecordings(callback: (rec: any) => void): void {
-  const bpmSamples = [75, 90, 94, 113, 122, 123, 65, 70, 99, 117];
+  const bpmSamples = [60, 121, 105, 77, 110, 75, 90, 94, 113, 122, 123, 65, 70, 99, 117];
   bpmSamples.forEach(async (bpm) => {
     try {
       const res = await fetch(`https://dxbtnxd6vjk30.cloudfront.net/media/tests/${bpm}bpm.mp3`);
