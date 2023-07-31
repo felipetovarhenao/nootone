b3d0e39cb5996ba534206d13d6787b8b2ac6d2dc
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-07-30 21:14:25 -0500

    Add comments to chord segmentation callback

--- src/utils/segmentChordEvents.ts
+++ src/utils/segmentChordEvents.ts
@@ -2,17 +2,26 @@ import { ChordEvent, ChordEventSegment } from "../types/music";
 import getDeepCopy from "./getDeepCopy";
 
 export default function segmentChordEvents(chordEvents: ChordEvent[], segmentDuration: number): ChordEventSegment[] {
+  // get number of segments based on total duration of chord event sequence
   const totalDuration = Math.max(...chordEvents.map((chord) => chord.onset + Math.max(...chord.notes.map((note) => note.duration))));
   const numSegments = Math.ceil(totalDuration / segmentDuration);
+
+  // initialize empty chord segments
   const chordSegments: ChordEventSegment[] = [...Array(numSegments).keys()].map((i) => ({ onset: i * segmentDuration, chords: [] }));
+
+  // iterate through each chord event and push to segment
   for (let i = 0; i < chordEvents.length; i++) {
     const chordEvent = getDeepCopy(chordEvents[i]);
+
+    // get segment information
     const segmentIndex = Math.floor(chordEvent.onset / segmentDuration + 1e-5);
     const segmentStart = segmentIndex * segmentDuration;
     const segmentEnd = segmentStart + segmentDuration;
 
+    // initialize new chord event in case note durations exceed segment end
     const nextChordEvent: ChordEvent = { onset: segmentEnd, notes: [] };
 
+    // check for cross-segment notes
     chordEvent.notes.forEach((note) => {
       const noteEnd = chordEvent.onset + note.duration;
       if (noteEnd > segmentEnd) {
@@ -21,6 +30,8 @@ export default function segmentChordEvents(chordEvents: ChordEvent[], segmentDur
         nextChordEvent.notes.push({ duration: noteDuration - note.duration, pitch: note.pitch, velocity: note.velocity });
       }
     });
+
+    // for cross-segment notes exist, get remaining segments recursively
     if (nextChordEvent.notes.length > 0) {
       segmentChordEvents([nextChordEvent], segmentDuration).forEach((seg) => {
         const segIndex = Math.floor(seg.onset / segmentDuration + 1e-5);
@@ -29,5 +40,7 @@ export default function segmentChordEvents(chordEvents: ChordEvent[], segmentDur
     }
     chordSegments[segmentIndex].chords.push(chordEvent);
   }
+
+  // filter out empty segments and sort by onset
   return chordSegments.filter((x) => x.chords.length > 0).sort((a, b) => a.onset - b.onset);
 }
